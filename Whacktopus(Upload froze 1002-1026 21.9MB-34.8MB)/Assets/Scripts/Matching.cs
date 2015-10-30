56cb1831cbebf8ef94cf4575daa63c1856b6e4fb
unknown <1302534@MC10747.uad.ac.uk>
2015-10-30 14:09:57 +0000

    fridaywork

---
+++ Whacktopus(Upload froze 1002-1026 21.9MB-34.8MB)/Assets/Scripts/Matching.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum state
+{
+	Under,
+	Stunned,
+	Jumping,
+	Cashed
+};
+
+public class Matching : MonoBehaviour {
+
+	private int clicks=0;
+	public state current=state.Under;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown(){
+		clicks++;
+		if (clicks == 1) {
+			current = state.Stunned;
+		}
+		if (clicks == 2) {
+			current = state.Cashed;
+		}
+	}
+
+	void Cashedin(){
+		Destroy(this.gameObject);
+	}
+
+	void OnTriggerEnter(Collider collision) {
+		if (collision.gameObject.tag == "Blue_octopus" 
+			&& this.gameObject.tag == "Blue_octopus"
+			&& collision.gameObject.GetComponent<Matching> ().current == state.Cashed
+			&& this.current == state.Stunned) {
+			collision.gameObject.GetComponent<Matching>().Cashedin();
+		}
+
+		if (clicks == 2) {
+			Cashedin ();
+		}
+	}
+}
