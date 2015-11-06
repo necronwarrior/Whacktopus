afa29235d6f5413814b60d6287fe77e89a84014c
unknown <1302534@MC10746.uad.ac.uk>
2015-11-06 15:23:52 +0000

    jumping kinda implemented

---
+++ Whacktopus(Upload froze 1002-1026 21.9MB-34.8MB)/Assets/Scripts/Jumping.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jumping : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+
+		if(this.GetComponent<States> ().current == state.Under)//(jumpButtonDown && !jumping)
+		{
+			//jumping = true;
+			StartCoroutine(JumpRoutine());
+
+		}
+	}
+
+	IEnumerator JumpRoutine(){
+		this.GetComponent<Rigidbody> ().AddForce(new Vector3(0.0f, 5.0f, 0.0f));
+		yield return new WaitForSeconds (1);//Random.Range(5,20));
+		this.GetComponent<States> ().current = state.Jumping;
+		this.GetComponent<Rigidbody> ().AddForce(new Vector3(0.0f, -5.0f, 0.0f));
+		this.GetComponent<Rigidbody> ().AddForce(new Vector3(0.0f, -5.0f, 0.0f));
+		yield return new WaitForSeconds (1);//Random.Range(5,20));
+		this.GetComponent<Rigidbody> ().AddForce(new Vector3(0.0f, 5.0f, 0.0f));
+		yield return new WaitForSeconds (Random.Range(5,20));
+		this.GetComponent<States> ().current = state.Under;
+	}
+}
---
+++ Whacktopus(Upload froze 1002-1026 21.9MB-34.8MB)/Assets/Scripts/States.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum state
+{
+	Under,
+	Stunned,
+	Jumping,
+	Cashed
+};
+
+public class States : MonoBehaviour {
+	public state current=state.Under;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
